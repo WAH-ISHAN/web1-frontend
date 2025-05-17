41392136ac05a1ceb2cd0633d6d444a58c686170
Wah-Ishan <wahasinduishan@gmail.com>
2025-05-17 14:56:14 +0530

    new update😍

--- src/pages/admin/AddProduct.jsx
+++ src/pages/admin/AddProduct.jsx
@@ -1,34 +1,11 @@
 
-import React from 'react';
-/* ProductID: ,
-    ProductName: ,
-    ProductDescription: ,
-    ProductPrice: ,
-    ProductlabelPrice: ,
-    ProductImage: ,
-    ProductStoke:,
-    ProductCategory:,
-    isAvailable:
-     */
 export function AddProduct(){
-return(
-    <div className='w-full h-full bg-white rounded-lg flex items-center justify-center'>
-        <div className='bg-white w-[500px] h-[600px] rounded-lg shoadow-lg felx justify-center items-center flex-col'>
-            <h1>Add Product</h1>
-            <input className=" w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
-            placeholder="ProductID" />
 
             <input className=" w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
             placeholder="ProductName" />
 
-            <textarea className=" w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
-            placeholder="ProductDescription" />
 
-            <input className=" w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
-            placeholder="ProductlabelPrice" />
 
-            <input className=" w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
-            placeholder="ProductImage" />
 
             <input className=" w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]"
             placeholder="ProductStoke" />
